f2b130d40f5e5bda26dca680c5bc28342d9336bf
Aleksandr <alex.iv.vad@gmail.com>
2022-07-18 15:38:56 +0300

    Update transformers.py
    Добавлено проверка is_fitted (fit or not)

--- ml_project/src/models/transformers.py
+++ ml_project/src/models/transformers.py
@@ -2,22 +2,29 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder
 import copy
 from scipy.sparse import hstack
+from sklearn.exceptions import NotFittedError
 
 
 class MultiOneHotEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, cat_columns):
         self.cat_columns = cat_columns
         self.ohes = {}
+        self.is_fitted_flg: bool = False
+
+    def is_fitted(self) -> None:
+        if not self.is_fitted_flg:
+            raise NotFittedError('MultiOneHotEncoder is not fitted')
 
     def fit(self, X, y=None):
         for column in self.cat_columns:
             ohe = OneHotEncoder(handle_unknown='ignore')
             ohe.fit(X[[column]])
             self.ohes[column] = copy.deepcopy(ohe)
-
+        self.is_fitted_flg = True
         return self
 
     def transform(self, X, y=None):
+        self.is_fitted()
         data = []
         for column in self.ohes.keys():
             data.append(self.ohes[column].transform(X[[column]]))
@@ -33,4 +40,5 @@ class FeatureSelector(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
+        # no fit required
         return X[self.num_columns].fillna(0)
